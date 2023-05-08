00e9122c1009fa0eed2c0b5b3763df8855f02736
Sina Falaki | سینا فلکی <123977796+SinaFalaki@users.noreply.github.com>
2023-05-08 15:01:43 +0330

    add files

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿Console.WriteLine("Hello, World!");
+#region فقط استارت کن
+
+
+Thread.Sleep(2000);
+Console.Beep(264, 125);
+Thread.Sleep(250);
+Console.Beep(264, 125);
+Thread.Sleep(125);
+Console.Beep(297, 500);
+Thread.Sleep(125);
+Console.Beep(264, 500);
+Thread.Sleep(125);
+Console.Beep(352, 500); 
+Thread.Sleep(125);
+Console.Beep(330, 1000);
+Thread.Sleep(250);
+Console.Beep(264, 125);
+Thread.Sleep(250);
+Console.Beep(264, 125);
+Thread.Sleep(125);
+Console.Beep(297, 500);
+Thread.Sleep(125);
+Console.Beep(264, 500);
+Thread.Sleep(125);
+Console.Beep(396, 500);
+Thread.Sleep(125);
+Console.Beep(352, 1000);
+Thread.Sleep(250);
+Console.Beep(264, 125);
+
+
+
+
+Thread.Sleep(125);
+Console.Beep(2642, 500);
+Thread.Sleep(125);
+Console.Beep(440, 500);
+Thread.Sleep(125);
+Console.Beep(352, 250);
+Thread.Sleep(125);
+Console.Beep(352, 125);
+Thread.Sleep(125);
+Console.Beep(330, 500);
+Thread.Sleep(125);
+Console.Beep(297, 1000);
+Thread.Sleep(250);
+Console.Beep(466, 125);
+Thread.Sleep(250);
+Console.Beep(466, 125);
+Thread.Sleep(125);
+Console.Beep(440, 500);
+Thread.Sleep(125);
+Console.Beep(352, 500);
+Thread.Sleep(125);
+Console.Beep(396, 500);
+Thread.Sleep(125);
+Console.Beep(352, 1000);
+#endregion
